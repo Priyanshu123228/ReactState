7d5a5c0e2b857f037474b18f67181f5e670df573
Priyanshu123228 <priyanshusaini1212@gmail.com>
2026-07-19 11:09:38 +0530

    TodoList

---
+++ src/TodoList.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import { v4 as uuidv4 } from "uuid";
+
+
+export default function TodoList() {
+    let [todos, setTodos] = useState([{task:"Sample task",id:uuidv4()}]);
+    let [newTodo,setNewTodo]=useState(""); //to track what input has taken .Stores whatever user types.
+
+    let addNewTask= ()=>{
+        setTodos([...todos,{task:newTodo,id:uuidv4()}]);
+        setNewTodo("");
+    }
+    let updateTodoValue=(event)=>{
+        setNewTodo(event.target.value);
+    }
+    return (
+        <div>
+            <input placeholder="Add a task" value={newTodo} onChange={updateTodoValue}></input>
+            <br></br><br></br>
+            <button onClick={addNewTask} >Add Task</button>
+            <br></br>
+            <br></br>
+            <br></br>
+            <hr></hr>
+            <h4>Task Todo</h4>
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li key={todo.id}>{todo.task}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
